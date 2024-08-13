ddf32b62be995b7fb3e8f451f5c7349f3e757d1d
Sabiha Anjum <drsabaanjum99@gmail.com>
2024-08-13 17:18:22 +0530

    Commit
    Commit

---
+++ TUF/Array/sorted_arr_search.py
@@ -0,0 +1,9 @@
+
+class Solution:
+    ##Complete this function
+    def searchInSorted(self,arr, N, K):
+        #Your code here
+        
+        if K in arr:
+            return 1
+        return -1
