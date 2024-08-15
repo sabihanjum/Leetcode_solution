fb8e7b4c64d3ac4ef092ca3c0ba4c17f3fcd326c
Sabiha Anjum <drsabaanjum99@gmail.com>
2024-08-15 15:45:35 +0530

    Commit
    Commit

---
+++ TUF/Array/max_consecutive_1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        cnt = 0
+        maxi = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                cnt += 1
+            else:
+                cnt = 0
+            maxi = max(maxi, cnt)
+        return maxi
