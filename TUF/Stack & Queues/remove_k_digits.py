924a20a05a2274d74c44500c4afc0d7a1d6e7525
Sabiha Anjum <drsabaanjum99@gmail.com>
2024-12-02 18:17:56 +0530

    Commit

--- TUF/Stack & Queues/remove_k_digits.py
+++ TUF/Stack & Queues/remove_k_digits.py
@@ -4,7 +4,7 @@ class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         """
         Remove 'k' digits from the given number 'num' to create the smallest possible number.
-        
+
         Args:
         num (str): The input number represented as a string.
         k (int): The number of digits to remove.
@@ -20,8 +20,8 @@ class Solution:
             # the current digit is smaller than the last digit in the stack,
             # remove the larger digit to make the number smaller.
             while k > 0 and stack and stack[-1] > c:
-                stack.pop()  # Remove the last digit
-                k -= 1  # Decrement the count of digits to remove
+                stack.pop()
+                k -= 1
             
             # Append the current digit to the stack
             stack.append(c)
@@ -30,9 +30,9 @@ class Solution:
         # remove them from the end of the stack.
         stack = stack[:len(stack) - k]
 
-        # Join the stack into a string to form the result
-        res = "".join(stack)
+        # Convert stack into a string and strip leading zeros
+        res = "".join(stack).lstrip("0")
 
-        # Convert to an integer to remove leading zeros, then back to a string.
-        # If the result is empty, return "0".
-        return str(int(res)) if res else "0"
+        # Return "0" if the result is empty
+        return res if res else "0"
+
