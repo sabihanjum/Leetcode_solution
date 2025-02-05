8703d2724bfd1a07e775e0ae6c3ebf544b342011
Sabiha Anjum <drsabaanjum99@gmail.com>
2025-02-05 18:28:43 +0530

    Find index of first occurrence in a String

---
+++ TUF/Strings/find_index_of_first_occurence_in_string.py
@@ -0,0 +1,12 @@
+"""Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack."""
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        m = len(haystack)
+        n = len(needle)
+
+        for i in range(m - n + 1):
+            if haystack[i:i + n] == needle:
+                return i
+
+        return -1
