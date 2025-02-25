cc8f9f4d6401550d4c22d22f9fb77c04cdad72ce
Sabiha Anjum <drsabaanjum99@gmail.com>
2025-02-25 18:34:33 +0530

    Cheapest flight within k spot

---
+++ TUF/Graphs/cheapest_flight_within_k_spot.py
@@ -0,0 +1,18 @@
+"""There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+
+You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1."""
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float("inf")] * n
+        prices[src] = 0
+
+        for i in range(k + 1):
+            tmpPrices = prices.copy()
+            for s, d, p in flights:
+                if prices[s] == float("inf"):
+                    continue
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
+            prices = tmpPrices
+        return -1 if prices[dst] == float("inf") else prices[dst]
